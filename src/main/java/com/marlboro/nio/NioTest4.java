4fdee10b8d320361ebdcbe1774399ea7fd05d3bb
tdw <dongwei.tian@tiejin.me>
2019-12-23 18:14:43 +0800

    nio tests

---
+++ src/main/java/com/marlboro/nio/NioTest4.java
@@ -0,0 +1,38 @@
+package com.marlboro.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioTest4 {
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inputStream = new FileInputStream("input.txt");
+        FileOutputStream outputStream = new FileOutputStream("output.txt");
+
+        FileChannel inputChannel = inputStream.getChannel();
+        FileChannel outputChannel = outputStream.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(512);
+
+        while(true) {
+            buffer.clear();
+
+            int read = inputChannel.read(buffer);
+
+            System.out.println("read: " + read);
+
+            if(-1 == read) {
+                break;
+            }
+
+            buffer.flip();
+
+            outputChannel.write(buffer);
+        }
+
+        inputChannel.close();
+        outputChannel.close();
+    }
+}
